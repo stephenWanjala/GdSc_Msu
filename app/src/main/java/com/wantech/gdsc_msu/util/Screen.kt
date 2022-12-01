e93b39c8de21224b1fa10e741d85bc1c8b54990a
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-02 01:18:55 +0300

    Screen.kt

--- app/src/main/java/com/wantech/gdsc_msu/util/Screen.kt
+++ app/src/main/java/com/wantech/gdsc_msu/util/Screen.kt
@@ -2,4 +2,6 @@ package com.wantech.gdsc_msu.util
 
 sealed class Screen(val route:String){
     object LoginAccountScreen:Screen(route = "login")
+    object SignInAccountScreen :Screen(route = "signIn")
+    object ForgotAccountPasswordScreen :Screen(route = "forgotPassword")
 }
