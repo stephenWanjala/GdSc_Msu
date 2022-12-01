59e8ef0fda47d953e038f0c4dfb9571bc557ced7
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-01 15:52:41 +0300

    initalize project

---
+++ app/src/main/java/com/wantech/gdsc_msu/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.wantech.gdsc_msu.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//val PrimaryDark = Color(0x5EFF5722)
+val PrimaryDark = Color(0xDBFF5722)
+val SecondaryDark = Color(0xFFE2E0E7)
+val TertiaryDark = Color(0xFFEFB8C8)
+val SurfaceDark = Color(0xFF956170)
+val SurfaceVariantDark = Color(0xB3403B3C)
+
+val PrimaryLight = Color(0xFF453D5C)
+val SecondaryLight = Color(0xDF817F86)
+val TertiaryLight = Color(0xFF7D5260)
+val SurfaceVariantLight = Color(0xC99B9496)
+val SurfaceLight = Color(0xC9E5CFD6)
---
+++ app/src/main/java/com/wantech/gdsc_msu/ui/theme/Theme.kt
@@ -0,0 +1,61 @@
+package com.wantech.gdsc_msu.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+private val DarkColorPalette = darkColors(
+    primary = PrimaryDark,
+    primaryVariant = TertiaryDark,
+    secondary = SecondaryDark,
+    surface = SurfaceDark,
+    background = SurfaceDark,
+
+)
+
+private val LightColorPalette = lightColors(
+    primary = PrimaryLight,
+    primaryVariant = TertiaryLight,
+    secondary = SecondaryLight,
+    background = SurfaceLight,
+    surface = SurfaceLight
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun GdSc_MsuTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor =
+                colors.surface.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
