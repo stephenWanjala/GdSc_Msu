d7ac95b79465613429633f0d72e7e147c5976ec5
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-06 23:18:32 +0300

    LoginUiEvent && LoginViewmodel skeleton

---
+++ app/src/main/java/com/wantech/gdsc_msu/feature_auth/login/presentation/LoginUiEvent.kt
@@ -0,0 +1,9 @@
+package com.wantech.gdsc_msu.feature_auth.login.presentation
+
+sealed class LoginUiEvent{
+    data class EnteredEmail(val value: String): LoginUiEvent()
+    data class EnteredPassword(val value: String): LoginUiEvent()
+    object TogglePasswordVisibility : LoginUiEvent()
+    object ToSignUpScreen : LoginUiEvent()
+    object Login : LoginUiEvent()
+}
--- app/src/main/java/com/wantech/gdsc_msu/feature_auth/login/presentation/LoginViewmodel.kt
+++ app/src/main/java/com/wantech/gdsc_msu/feature_auth/login/presentation/LoginViewmodel.kt
@@ -1,6 +1,28 @@
 package com.wantech.gdsc_msu.feature_auth.login.presentation
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel: ViewModel() {
+    private val _state = mutableStateOf(LoginUiState())
+    val state: State<LoginUiState> = _state
+
+    fun onEvent(event: LoginUiEvent) {
+        when (event) {
+            is LoginUiEvent.EnteredEmail -> {
+                _state.value = _state.value.copy(email = event.value)
+            }
+            is LoginUiEvent.EnteredPassword -> {
+                _state.value = _state.value.copy(password = event.value)
+            }
+            is LoginUiEvent.TogglePasswordVisibility -> {
+                _state.value = _state.value.copy(isPasswordVisible = !state.value.isPasswordVisible)
+            }
+            is LoginUiEvent.ToSignUpScreen -> {
+            }
+            is LoginUiEvent.Login -> {
+            }
+        }
+    }
 }
