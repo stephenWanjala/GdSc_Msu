5ef40299af717ee81910b032e5709de01906e6fa
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-06 22:58:29 +0300

    Dagger hilt dependencies

--- app/src/main/java/com/wantech/gdsc_msu/GdscApp.kt
+++ app/src/main/java/com/wantech/gdsc_msu/GdscApp.kt
@@ -1,7 +1,7 @@
 package com.wantech.gdsc_msu
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-
-
+@HiltAndroidApp
 class GdscApp :Application()
--- app/src/main/java/com/wantech/gdsc_msu/feature_auth/login/domain/model/RegisterUserModel.kt
+++
@@ -1,7 +0,0 @@
-package com.wantech.gdsc_msu.feature_auth.login.domain.model
-
-data class RegisterUserModel(
-    val userName: String,
-    val email: String,
-    val password: String
-)
