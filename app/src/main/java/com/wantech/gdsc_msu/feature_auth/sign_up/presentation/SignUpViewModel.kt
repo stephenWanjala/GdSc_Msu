ab5325d9a1c6c5accdca1fdabb06528f0b5b5231
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-06 16:16:57 +0300

    SignUpViewModel skeleton-> onVent

--- app/src/main/java/com/wantech/gdsc_msu/feature_auth/sign_up/presentation/SignUpViewModel.kt
+++ app/src/main/java/com/wantech/gdsc_msu/feature_auth/sign_up/presentation/SignUpViewModel.kt
@@ -1,6 +1,39 @@
 package com.wantech.gdsc_msu.feature_auth.sign_up.presentation
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel:ViewModel() {
+    private val _state = mutableStateOf(SignUpUIState())
+    val state: State<SignUpUIState> = _state
+
+
+    fun onEvent(event: SignupEvent) {
+        when (event) {
+            is SignupEvent.EnteredUsername -> {
+                _state.value = _state.value.copy(userName = event.value)
+            }
+
+            is SignupEvent.EnteredEmail -> {
+                _state.value = _state.value.copy(email = event.value)
+            }
+
+            is SignupEvent.EnteredPassword -> {
+                _state.value = _state.value.copy(password = event.value)
+            }
+
+            is SignupEvent.TogglePasswordVisibility -> {
+                _state.value = _state.value.copy(isPasswordVisible = !state.value.isPasswordVisible)
+            }
+
+            is SignupEvent.Signup -> {
+            }
+
+            is SignupEvent.ToSignInScreen -> {
+            }
+
+        }
+    }
+
 }
