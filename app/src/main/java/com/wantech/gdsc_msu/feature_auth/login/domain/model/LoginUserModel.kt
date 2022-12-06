6f26d5f983466cc78b4811a62e019a16514169a8
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-06 22:59:13 +0300

    LoginUiState && LoginUiUserModel

---
+++ app/src/main/java/com/wantech/gdsc_msu/feature_auth/login/domain/model/LoginUserModel.kt
@@ -0,0 +1,6 @@
+package com.wantech.gdsc_msu.feature_auth.login.domain.model
+
+data class LoginUserModel(
+    val email: String,
+    val password: String
+)
---
+++ app/src/main/java/com/wantech/gdsc_msu/feature_auth/login/presentation/LoginUiState.kt
@@ -0,0 +1,21 @@
+package com.wantech.gdsc_msu.feature_auth.login.presentation
+
+data class LoginUiState(
+    val email: String = "",
+    val password: String = "",
+    val isPasswordVisible: Boolean = false,
+    val isEmailError: EmailError? = null,
+    val isPasswordError: PasswordError? = null,
+    val isLoginButtonEnabled: Boolean = false,
+) {
+    sealed class EmailError {
+        object FieldEmpty : EmailError()
+        object InvalidEmail: EmailError()
+    }
+    sealed class PasswordError {
+        object FieldEmpty: PasswordError()
+        object InvalidPassword : PasswordError()
+        object InputTooShort : PasswordError()
+    }
+}
+
