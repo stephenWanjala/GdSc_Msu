3be1928bdac69a6c03d321010d35c6342b26ebb0
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-10 13:59:39 +0300

    utility class

--- app/src/main/java/com/wantech/gdsc_msu/util/Resource.kt
+++ app/src/main/java/com/wantech/gdsc_msu/util/Resource.kt
@@ -1,7 +1,8 @@
 package com.wantech.gdsc_msu.util
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(data: T? = null, message: String) : Resource<T>(data, message)
-    class Loading<T> : Resource<T>()
+typealias SimpleResource = Resource<Unit>
+
+sealed class Resource<T>(val data: T? = null, val uiText: UiText? = null) {
+    class Success<T>(data: T?): Resource<T>(data)
+    class Error<T>(uiText: UiText, data: T? = null): Resource<T>(data, uiText)
 }
