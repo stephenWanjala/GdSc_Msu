2ad1207302f54912bc71f319766868c49e2599b4
wanjala stephen <stephenwanjala145@gmail.com>
2022-12-22 01:55:53 +0300

    auth repository

---
+++ app/src/main/java/com/wantech/gdsc_msu/core/domain/repositoryImpl/AuthRepositoryImpl.kt
@@ -0,0 +1,67 @@
+package com.wantech.gdsc_msu.core.domain.repositoryImpl
+
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.wantech.gdsc_msu.core.data.repository.AuthRepository
+import com.wantech.gdsc_msu.util.Resource
+import com.wantech.gdsc_msu.util.UiText
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class AuthRepositoryImpl @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+) : AuthRepository {
+    override suspend fun getUserId(): Flow<String> = flow {
+        firebaseAuth.currentUser?.uid?.let {
+            emit(it)
+        }
+    }
+
+
+    override suspend fun signUpWithEmailAndPassword(
+        email: String,
+        password: String
+    ): Flow<Resource<AuthResult>> {
+        return flow {
+            emit(Resource.Loading())
+            emit(
+                Resource.Success(
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+                )
+            )
+        }.catch {
+            emit(Resource.Error(UiText.DynamicString(it.message ?: "Unknown Error")))
+        }
+    }
+
+    override suspend fun signInWithEmailAndPassword(
+        email: String,
+        password: String
+    ): Flow<Resource<AuthResult>> {
+        return flow {
+            emit(Resource.Loading())
+            emit(Resource.Success(firebaseAuth.signInWithEmailAndPassword(email, password).await()))
+        }.catch {
+            emit(Resource.Error(UiText.DynamicString(it.message ?: "Unknown Error")))
+        }
+
+    }
+
+    override  fun isCurrentUserExist(): Flow<Boolean> = flow {
+        emit(firebaseAuth.currentUser != null)
+    }
+
+
+    override suspend fun getCurrentUserEmail(): Flow<String> {
+        return flow {
+            firebaseAuth.currentUser?.email?.let {
+                emit(it)
+            }
+        }
+    }
+
+    override suspend fun signOut() = firebaseAuth.signOut()
+}
