bfd8a30d138304d90f880773b4470a79cb59baab
wanjala stephen <stephenwanjala145@gmail.com>
2023-01-27 20:28:12 +0300

    NewsUpdatesScreen -> add Tab

--- app/src/main/java/com/wantech/gdsc_msu/feature_main/news/presentation/components/NewsUpdatesScreen.kt
+++ app/src/main/java/com/wantech/gdsc_msu/feature_main/news/presentation/components/NewsUpdatesScreen.kt
@@ -3,9 +3,9 @@ package com.wantech.gdsc_msu.feature_main.news.presentation.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -15,24 +15,33 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun NewsUpdatesScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) {
-        val unUsedPadding = it.calculateTopPadding()
+fun NewsUpdatesScreen(
+    tabData: List<String> = listOf("Groups", "News")
+) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+
         val scope = rememberCoroutineScope()
-        val tabData = listOf("Groups", "News")
-        Column(modifier = Modifier.fillMaxWidth()) {
-            val pagerState = rememberPagerState()
-            val tabIndex = pagerState.currentPage
-            TabRow(selectedTabIndex = tabIndex, indicator = { tabPositions ->
+        val pagerState = rememberPagerState()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+        ) {
+            var tabIndex by remember {
+                mutableStateOf(0)
+            }
+            TabRow(selectedTabIndex = pagerState.currentPage, indicator = { tabPositions ->
                 TabRowDefaults.Indicator(
-                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
+                    height = TabRowDefaults.IndicatorHeight * 1.5F
                 )
             }) {
-                tabData.forEachIndexed { index,tab ->
+                tabData.forEachIndexed { index, tab ->
                     Tab(
                         text = { Text(text = tab) },
                         selected = tabIndex == index,
                         onClick = {
+                            tabIndex = index
                             scope.launch {
                                 pagerState.animateScrollToPage(index)
                             }
@@ -41,9 +50,10 @@ fun NewsUpdatesScreen() {
                 }
             }
             HorizontalPager(
-                count = tabData.size,
+                modifier = Modifier.weight(1f),
                 state = pagerState,
-            ) { page ->
+                count = tabData.size,
+            ) {
                 // TODO: page content
             }
 
