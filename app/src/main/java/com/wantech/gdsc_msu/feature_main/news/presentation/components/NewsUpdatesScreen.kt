d59c982d28e49a074822b132681d7c7cbb632ed8
wanjala stephen <stephenwanjala145@gmail.com>
2023-01-27 19:43:46 +0300

    NewsUpdatesScreen -> add Tab

--- app/src/main/java/com/wantech/gdsc_msu/feature_main/news/presentation/components/NewsUpdatesScreen.kt
+++ app/src/main/java/com/wantech/gdsc_msu/feature_main/news/presentation/components/NewsUpdatesScreen.kt
@@ -3,18 +3,50 @@ package com.wantech.gdsc_msu.feature_main.news.presentation.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun NewsUpdatesScreen   () {
+fun NewsUpdatesScreen() {
     Scaffold(modifier = Modifier.fillMaxSize()) {
-        val unUsedPadding =it.calculateTopPadding()
-
+        val unUsedPadding = it.calculateTopPadding()
+        val scope = rememberCoroutineScope()
+        val tabData = listOf("Groups", "News")
         Column(modifier = Modifier.fillMaxWidth()) {
-            Text(text = "News Notifications")
+            val pagerState = rememberPagerState()
+            val tabIndex = pagerState.currentPage
+            TabRow(selectedTabIndex = tabIndex, indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }) {
+                tabData.forEachIndexed { index,tab ->
+                    Tab(
+                        text = { Text(text = tab) },
+                        selected = tabIndex == index,
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        }
+                    )
+                }
+            }
+            HorizontalPager(
+                count = tabData.size,
+                state = pagerState,
+            ) { page ->
+                // TODO: page content
+            }
+
         }
     }
 }
